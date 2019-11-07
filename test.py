8efe1d6b74a445aaea9ea0094a2b2e0414fe8aea
siddht4 <dhar.75.ap@gmail.com>
2019-11-07 13:06:15 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,7 +4,12 @@ with alive_bar(len(items)) as bar:   # declare your expected total
     for item in items:               # iterate as usual
         # process each item
         bar() 
-from alive_progress import showtime
-showtime()
+
         
-        
+import time
+with alive_bar(1000) as bar:
+    for i in range(1000):
+        if i%100==0:
+            print('100')
+         time.sleep(0.01)
+        bar()

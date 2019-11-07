d75372d2659d9bb1903128efa145c8b98f2d265a
siddht4 <dhar.75.ap@gmail.com>
2019-11-07 13:01:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,7 @@ with alive_bar(len(items)) as bar:   # declare your expected total
     for item in items:               # iterate as usual
         # process each item
         bar() 
+from alive_progress import showtime
+showtime()
+        
+        
